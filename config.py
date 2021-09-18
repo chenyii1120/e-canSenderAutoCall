073d7ed74bd456b4f3afaabb94b614ef914d7625
Joseph Chen <chenyii1120@gmail.com>
2021-09-18 09:53:35 +0800

    no message

---
+++ config.py
@@ -0,0 +1,18 @@
+# 建議參考README.md文件設定
+BROWSER_DRIVER_PATH = "ChromeDriver/chromedriver.exe"
+
+# 宅配通EDI客代
+ID = "4669080101"
+
+# 宅配通EDI密碼
+PASSWORD = "ab271020"
+
+# 排程執行時間 (24小時制，HH:MM冒號分隔，不足兩位數請補0)
+TIME = "16:05"
+
+# log檔內的時間格式，不建議變更
+TIME_FORMAT = "%Y-%m-%d %H:%M:%S"
+
+# 崩潰報告檔名格式，不建議變更
+CRASH_REPORT_NAME_FORMAT = "%Y%m%d_%H%M"
+
---
+++ try.py
@@ -0,0 +1,4 @@
+import datetime
+import config
+
+print(datetime.datetime.now().strftime(config.TIME_FORMAT))
