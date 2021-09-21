a0cdc7dee8f4d9f60b76f8193f1349a52a9b124b
Joseph Chen <chenyii1120@gmail.com>
2021-09-21 18:09:05 +0800

    更新計時器開關及間隔設定

--- lib/clock.py
+++ lib/clock.py
@@ -4,19 +4,33 @@ import time
 
 
 def check_time():
-    run_time = config.TIME.split(":")
+    # 判斷是否使用內建計時器
+    if not config.USE_INNER_TIMER:
+        return True
+    else:
 
-    while True:
-        now = datetime.datetime.now()
-        now_inform = now.strftime(config.TIME_FORMAT)
+        run_time = config.TARGET_TIME.split(":")
+        interval = int(config.INNER_TIMER_CHECK_INTERVAL)
 
-        if now.hour == int(run_time[0]) and now.minute >= int(run_time[1]):
-            return True
-            break
+        # 若config內間隔設定為-1則停用間隔
+        if interval == -1:
+            interval = 0
+        elif interval == 0:
+            interval = 1
 
-        else:
-            print(f"現在時間：{now_inform}，排程預計於每日 {run_time[0]}:{run_time[1]} 執行，每 5 分鐘檢查一次")
-            time.sleep(300)
+        # 判斷時間迴圈
+        while True:
+            now = datetime.datetime.now()
+            now_inform = now.strftime(config.TIME_FORMAT)
+
+            if now.hour == int(run_time[0]) and now.minute >= int(run_time[1]):
+                return True
+
+            else:
+                print(f"現在時間：{now_inform}，排程預計於每日 {run_time[0]}:{run_time[1]} 執行，每 {interval} 分鐘檢查一次")
+
+                time.sleep(60 * interval)
+
 
 def get_now():
     return datetime.datetime.now()
