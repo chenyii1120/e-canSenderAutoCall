3903a95822c620dbf3f3048aa1c219a652c0c0ff
Joseph Chen <chenyii1120@gmail.com>
2021-09-21 17:46:40 +0800

    新增預計功能(未實裝)

--- config.py
+++ config.py
@@ -16,5 +16,18 @@ TIME_FORMAT = "%Y-%m-%d %H:%M:%S"
 # 崩潰報告檔名格式，不建議變更
 CRASH_REPORT_NAME_FORMAT = "%Y%m%d_%H%M%S"
 
-# 是否跳過週末 (True/False)
+# TODO: 是否跳過週末 (True/False)
 SKIP_WEEKEND = True
+
+# TODO: 是否需生成log檔(建議為True)
+NEED_LOG = True
+
+# TODO: 是否啟用程序內的計時器
+#   (建議為 True，若設置為 False 時程式一開啟將直接啟動 bot，若需定時則需搭配系統內建排程功能)
+INNER_TIMER = True
+
+# TODO: 若派件失敗是否自動重試
+AUTO_RETRY = True
+
+# TODO: 自動重試次數
+RETRY_TIMES = 0
