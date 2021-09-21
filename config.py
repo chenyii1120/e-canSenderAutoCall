c55fc7a465769dd2b4eb41ca3ba01b841ab54c6b
Joseph Chen <chenyii1120@gmail.com>
2021-09-21 18:08:30 +0800

    新增預計功能(未實裝)

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ ID = "4669080101"
 PASSWORD = "ab271020"
 
 # 排程執行時間 (24小時制，HH:MM冒號分隔，不足兩位數請補0)
-TIME = "16:05"
+TARGET_TIME = "16:05"
 
 # log檔內的時間格式，不建議變更
 TIME_FORMAT = "%Y-%m-%d %H:%M:%S"
@@ -22,9 +22,12 @@ SKIP_WEEKEND = True
 # TODO: 是否需生成log檔(建議為True)
 NEED_LOG = True
 
-# TODO: 是否啟用程序內的計時器
-#   (建議為 True，若設置為 False 時程式一開啟將直接啟動 bot，若需定時則需搭配系統內建排程功能)
-INNER_TIMER = True
+# 是否啟用程序內的計時器
+# (建議為 True，若設置為 False 時程式一開啟將直接啟動 bot，若需定時則需搭配系統內建排程功能)
+USE_INNER_TIMER = True
+
+# 內建計時器檢查間隔 (分鐘)
+INNER_TIMER_CHECK_INTERVAL = 5
 
 # TODO: 若派件失敗是否自動重試
 AUTO_RETRY = True
